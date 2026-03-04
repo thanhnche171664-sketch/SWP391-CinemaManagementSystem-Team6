26cbdda206dcd9657314824aa590ff682192eeee
thanhnche171664-sketch <ThanhNCHE171664@fpt.edu.vn>
2026-03-04 23:11:00 +0700

    fix csrf

---
+++ SpringBoot/src/main/java/com/swp391/team6/cinema/config/CsrfControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.swp391.team6.cinema.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class CsrfControllerAdvice {
+
+    @ModelAttribute("_csrf")
+    public CsrfToken csrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    }
+}
